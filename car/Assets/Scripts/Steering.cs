83196da65023c472d4b308238a39077afdbf1fad
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-04-12 10:54:29 -0600

    ,knkl;h;jh;khh;ughipugiyfbbbbbbbbbb

--- car/Assets/Scripts/Steering.cs
+++ car/Assets/Scripts/Steering.cs
@@ -1,5 +1,7 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class Steering : MonoBehaviour
@@ -8,27 +10,59 @@ public class Steering : MonoBehaviour
     public GameObject wheel2;
     public GameObject wheel3;
     public GameObject wheel4;
-    public float turnAngle = 10;
+    public float turnSpeed = 0.2f;
+    private Rigidbody rb;
+    public float tireAngle = 0;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody>();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        float ang = 0;
 
-        if (Input.GetKey(KeyCode.A)) ang -= turnAngle;
-        if (Input.GetKey(KeyCode.D)) ang += turnAngle;
+        float speed = transform.InverseTransformDirection(rb.velocity).x;
+
+        float rotPer2nd = Input.GetAxis("Horizontal") * turnSpeed;
+
+        if (rotPer2nd == 0)
+        {
+            tireAngle = 0;
+            Debug.Log("no Input");
+            return;
+        }
+
+        float c = speed * rotPer2nd;
+
+        float r = c / 2 * Mathf.PI;
+
+        float angleRad = Mathf.Atan2(2, r);
+
+        tireAngle = Mathf.Rad2Deg * angleRad;
+
+        Debug.Log(tireAngle);
+    }
+
+    private void FixedUpdate()
+    {
+        updateTires();
+    }
+
+
+    private void updateTires()
+    {
         Vector3 rot = wheel1.transform.localEulerAngles;
-        rot.y = ang;
+
+        rot.y = tireAngle;
+
         wheel1.transform.localEulerAngles = rot;
         wheel2.transform.localEulerAngles = rot;
-        wheel3.transform.localEulerAngles = rot/-2;
-        wheel4.transform.localEulerAngles = rot/-2;
+        wheel3.transform.localEulerAngles = rot;
+        wheel4.transform.localEulerAngles = rot;
     }
 }
