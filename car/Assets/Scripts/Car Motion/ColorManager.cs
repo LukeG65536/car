14ecb5f8b4cae68716b32606f8649c7d6ed16f12
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-05-16 11:09:47 -0600

    color fffffrrfrfrfrfrfrffrfrffrfrfrffrfrffrfrff

--- car/Assets/Scripts/Car Motion/ColorManager.cs
+++ car/Assets/Scripts/Car Motion/ColorManager.cs
@@ -1,6 +1,7 @@
-using System.Collections;
+using System;
 using System.Collections.Generic;
 using Unity.Netcode;
+using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -8,9 +9,12 @@ public class ColorManager : NetworkBehaviour
 {
     public Material mat;
     public Color color;
+
+    public MeshRenderer[] meshRenderers;
     // Start is called before the first frame update
     void Start()
     {
+        mat = new Material(mat);
         if (!IsOwner) return;
         GameObject.Find("ColorManager").GetComponent<buttonThingyColor>().colorManager = this;
     }
@@ -24,25 +28,28 @@ public class ColorManager : NetworkBehaviour
     public void setColorRandom()
     {
         if (!IsOwner) return;
-        color = Random.ColorHSV();
+        color = UnityEngine.Random.ColorHSV();
         color.a = 1;
         mat.color = color;
-        setColorServerRpc(color.r, color.g, color.b);
+        setColorServerRpc(color.r, color.g, color.b, NetworkObjectId);
     }
 
     [ServerRpc]
-    public void setColorServerRpc(float r, float g, float b)
+    public void setColorServerRpc(float r, float g, float b,ulong id)
     {
-        setColorClientRpc(r, g, b);
+        setColorClientRpc(r, g, b, id);
     }
 
     [ClientRpc]
-    public void setColorClientRpc(float r, float g, float b)
+    public void setColorClientRpc(float r, float g, float b, ulong id)
     {
-        if (IsOwner) return;
+        if (NetworkObjectId != id) return;
         color = new Color(r, g, b);
         mat.color = color;
+
+        foreach(MeshRenderer renderer in meshRenderers)
+        {
+            renderer.material = mat;
+        }
     }
-
-
 }
