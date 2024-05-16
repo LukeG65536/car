4d3d9061f762bdcf080f702858c3f5c3f390403a
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-05-16 09:35:17 -0600

    askjh;vkajevlaobindbbbbbbbb

--- car/Assets/Scripts/Car Motion/ColorManager.cs
+++ car/Assets/Scripts/Car Motion/ColorManager.cs
@@ -4,13 +4,14 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ColorManager : MonoBehaviour
+public class ColorManager : NetworkBehaviour
 {
     public Material mat;
     public Color color;
     // Start is called before the first frame update
     void Start()
     {
+        if (!IsOwner) return;
         GameObject.Find("ColorManager").GetComponent<buttonThingyColor>().colorManager = this;
     }
 
@@ -22,21 +23,23 @@ public class ColorManager : MonoBehaviour
 
     public void setColorRandom()
     {
+        if (!IsOwner) return;
         color = Random.ColorHSV();
         color.a = 1;
         mat.color = color;
-        setColorServer(color.r, color.g, color.b);
+        setColorServerRpc(color.r, color.g, color.b);
     }
 
     [ServerRpc]
-    public void setColorServer(float r, float g, float b)
+    public void setColorServerRpc(float r, float g, float b)
     {
-        setColorClient(r, g, b);
+        setColorClientRpc(r, g, b);
     }
 
     [ClientRpc]
-    public void setColorClient(float r, float g, float b)
+    public void setColorClientRpc(float r, float g, float b)
     {
+        if (IsOwner) return;
         color = new Color(r, g, b);
         mat.color = color;
     }
