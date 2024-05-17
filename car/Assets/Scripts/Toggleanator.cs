6e23122d5082db4527be17c6e869012874d5e80a
LukeG65536 <116130542+LukeG65536@users.noreply.github.com>
2024-05-17 09:22:50 -0600

    hhoo

---
+++ car/Assets/Scripts/Toggleanator.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Toggleanator : MonoBehaviour
+{
+    public bool current = true;
+    public KeyCode key = KeyCode.Escape;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            current = !current;
+            Debug.Log(current);
+            foreach(var obj in transform.GetComponentsInChildren<Button>())
+            {
+                obj.enabled = current;
+                obj.GetComponent<Image>().enabled = current;
+                obj.transform.GetChild(0).gameObject.SetActive(current);
+            }
+        }
+    }
+}
